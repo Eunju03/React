ab3bd4730fa9b13332afdbbb209a3e0080a79c59
Eunju03 <plasou03@naver.com>
2022-09-07 20:44:56 +0900

    map : div들을 반복문으로 줄이고 싶을때

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ import { useState } from 'react';
 function App() {
 
   let [글제목, 글제목변경] = useState(['남자 코트 추천','강남 우동맛집','파이썬독학']);
-  let [따봉, 따봉변경] = useState(0);
+  let [따봉, 따봉변경] = useState([0,0,0]);
   let [modal, setModel] = useState(false);
-
+  
 
   return (
     <div className="App">
@@ -20,10 +20,10 @@ function App() {
       <button onClick={()=>{ let copy = [...글제목]; copy[0] = '여자 코트 추천'; 글제목변경(copy) }}>
         글수정 </button>
 
-      <button onClick={()=>{ let copy1 = {...글제목}; copy1.sort(); 글제목변경(copy1) }}>
-        가나다순정렬 </button>
+      {/* <button onClick={()=>{ let copy1 = {...글제목}; copy1.sort(); 글제목변경(copy1) }}>
+        가나다순정렬 </button> */}
 
-      <div className="list">
+      {/* <div className="list">
         <h4>{ 글제목[0] } <span onClick={()=>{ 따봉변경(따봉+1) }}>👍</span> 
         { 따봉 }  </h4>
         <p>2월 17일 발행</p>
@@ -35,12 +35,28 @@ function App() {
       <div className="list">
         <h4 onClick={()=>{ setModel(true) }} >{ 글제목[2] }</h4>
         <p>2월 17일 발행</p>
-      </div>
+      </div> */}
 
       {
         modal == true ? <Modal/> : null
       }
-      if sta
+
+      {
+        글제목.map(function(a, i){
+          return (
+            <div className="list" key={i}>
+              <h4>{ 글제목[i] }
+              <span onClick={()=>{ 
+                let copy2 = [...따봉]; 
+                copy2[i] = copy2[i] + 1;
+                따봉변경(copy2)
+                }}>👍</span> 
+              { 따봉[i] }</h4>
+              <p>2월 17일 발행</p>
+            </div>
+            )
+        })
+      }
 
     </div>
   );
