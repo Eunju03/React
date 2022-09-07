e6edddc3e2852a624a52b78af35cc7d3217cab1d
Eunju03 <plasou03@naver.com>
2022-09-07 19:40:12 +0900

    Component : 많은 div들을 한 단어로 줄이고 싶으면

--- src/App.js
+++ src/App.js
@@ -17,11 +17,11 @@ function App() {
         <h4>ReactBlog</h4>
       </div>
 
-      <button onClick={()=>{
-        let copy = [...글제목];
-        copy[0] = '여자 코트 추천' 
-        글제목변경(copy);
-      }}>글수정</button>
+      <button onClick={()=>{ let copy = [...글제목]; copy[0] = '여자 코트 추천'; 글제목변경(copy) }}>
+        글수정 </button>
+
+      <button onClick={()=>{ let copy1 = {...글제목}; copy1.sort(); 글제목변경(copy1) }}>
+        가나다순정렬 </button>
 
       <div className="list">
         <h4>{ 글제목[0] } <span onClick={()=>{ 따봉변경(따봉+1) }}>👍</span> 
@@ -36,8 +36,21 @@ function App() {
         <h4>{ 글제목[2] }</h4>
         <p>2월 17일 발행</p>
       </div>
+
+      <Modal></Modal>
+
     </div>
   );
+}
+
+function Modal() {
+  return (
+    <div className='modal'>
+      <h4>제목</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
+  )
 }
 
 export default App;
