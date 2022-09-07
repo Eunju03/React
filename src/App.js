73751aab1f8a618704c5bc66a095ea5be2eb5270
Eunju03 <plasou03@naver.com>
2022-09-07 20:17:06 +0900

    동적인 UI 만드는 법

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import { useState } from 'react';
 
 function App() {
 
-  let post = '강남 우동 맛집';
   let [글제목, 글제목변경] = useState(['남자 코트 추천','강남 우동맛집','파이썬독학']);
   let [따봉, 따봉변경] = useState(0);
+  let [modal, setModel] = useState(false);
 
 
   return (
@@ -29,15 +29,18 @@ function App() {
         <p>2월 17일 발행</p>
       </div>
       <div className="list">
-        <h4>{ 글제목[1] }</h4>
+        <h4 onClick={()=>{ setModel(!modal) }}>{ 글제목[1] }</h4>
         <p>2월 17일 발행</p>
       </div>
       <div className="list">
-        <h4>{ 글제목[2] }</h4>
+        <h4 onClick={()=>{ setModel(true) }} >{ 글제목[2] }</h4>
         <p>2월 17일 발행</p>
       </div>
 
-      <Modal></Modal>
+      {
+        modal == true ? <Modal/> : null
+      }
+      if sta
 
     </div>
   );
